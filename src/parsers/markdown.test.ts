0818687a4ff8180009ead7b367a5d3bb28e19c53
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-04-03 16:02:11 -0700

    Add extra test

--- src/parsers/markdown.test.ts
+++ src/parsers/markdown.test.ts
@@ -95,4 +95,35 @@ it('parses returns "undefined" when header is not defined', () => {
     })
 })
 
-it.todo('can parse everything simultaneously')
+it('can parse everything simultaneously', () => {
+    expect(
+        parseMarkdown([
+            '% chromatic_transposition(note, interval, alteration, simplify)',
+            '',
+            'And a description of how the code works',
+            '',
+            '@ first (string) Text of the first parameter',
+            '@ [optional] (any) Optional parameters to be called whatever',
+            '',
+            ': (number) Number of whatever is done or nil if an error occurred',
+        ])
+    ).toMatchObject({
+        markdown: [
+            '## chromatic_transposition',
+            '',
+            '```lua',
+            'chromatic_transposition(note, interval, alteration, simplify)',
+            '```',
+            '',
+            'And a description of how the code works',
+            '',
+            '| Input | Type | Description |',
+            '| `first` | `string` | Text of the first parameter |',
+            '| `optional` (optional) | `any` | Optional parameters to be called whatever |',
+            '',
+            '| Output type | Description |',
+            '| `number` | Number of whatever is done or nil if an error occurred |',
+        ].join('\n'),
+        header: 'chromatic_transposition',
+    })
+})
